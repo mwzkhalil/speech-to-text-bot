272fe053b2c9d5e51cbeb5adf4086a2f77d107d5
mahwiz khalil <77918472+mwzkhalil@users.noreply.github.com>
2024-06-02 09:31:56 -0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, request, jsonify
 from audio_recorder_streamlit import audio_recorder
 from faster_whisper import WhisperModel
-from openai import OpenAI
+from transformers import AutoTokenizer
+import transformers
+
 
 app = Flask(__name__)
 
 ######### Transcriber ###########
-
 model_size = "large-v3"
 model = WhisperModel(model_size, device="cpu", compute_type="int8")
 
@@ -20,17 +21,69 @@ def transcribe_audio(audio_file_path, beam_size=5):
 
     return transcribed_text
 
-client = OpenAI(api_key="")
+#specify the model you want to use
+model = "meta-llama/Meta-Llama-3-8B"
+
+#load the pretrained tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model)
+
+#make it ready for text generation
+pipeline = transformers.pipeline(
+    "text-generation",
+    model=model,
+    torch_dtype=torch.float16,
+    device_map="auto",
+)
+
+def get_response_llama2(prompt):
+
+  sequences = pipeline(prompt,
+      do_sample=True,
+      return_full_text=False,
+      top_k=10,
+      num_return_sequences=1,
+      eos_token_id=tokenizer.eos_token_id,
+      max_length=4096,
+  )
+  return sequences[0]['generated_text']
+
+systems = {"role": "system", "content": "You are an assistant to retrieve important information from a given message. Each variable should have its own line."}
+    # Ensure the input is a list of messages
+    if isinstance(inp, str):
+        user_message = {"role": "user", "content": inp}
+        new_inp = [systems, user_message]
+    elif isinstance(inp, list):
+        new_inp = [systems] + inp
+    else:
+        raise ValueError("Input should be a string or a list of messages")
+    
+    # print("inp : \n", new_inp)
+    # new_inp = inp
+    # new_inp.insert(0,systems)
+    # print("inp : \n ",new_inp)
+    completion = get_response_llama2(
+    new_inp
+    )
+    return completion
 
 # Analyze transcription function
 def analyze_transcription(transcription):
-    response = client.chat.ChatCompletion.create(
-        model="gpt-4o",
-        messages=[
-            {"role": "system", "content": "You are an assistant to retrieve important information from a given message. Each variable should have its own line."},
-            {"role": "user", "content": "Retrieve name, issue and phone number from the following text: {}".format(transcription)}
-            ],
-        temperature=0.2
+    systems = {"role": "system", "content": "You are an assistant to retrieve important information from a given message. Each variable should have its own line."}
+    # Ensure the input is a list of messages
+    if isinstance(inp, str):
+        user_message = {"role": "user", "content": inp}
+        new_inp = [systems, user_message]
+    elif isinstance(inp, list):
+        new_inp = [systems] + inp
+    else:
+        raise ValueError("Input should be a string or a list of messages")
+    
+    # print("inp : \n", new_inp)
+    # new_inp = inp
+    # new_inp.insert(0,systems)
+    # print("inp : \n ",new_inp)
+    response = get_response_llama2(
+    new_inp
     )
     return response
 
